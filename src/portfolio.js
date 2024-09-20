1f312826695db54708786980e454d4251be33179
megaraph <47447126+megaraph@users.noreply.github.com>
2024-09-20 18:50:13 +0800

    add test portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,11 +20,9 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
-  ),
+  username: "Raphael Murillo",
+  title: "Hi all, I'm Raph",
+  subTitle: emoji("Skibidi toilet 🚽. I love libby"),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
