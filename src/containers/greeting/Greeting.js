ca600bbd5e8ecb8a5a0bd364376ca3903cb73547
megaraph <47447126+megaraph@users.noreply.github.com>
2024-09-21 10:32:08 +0800

    update responsiveness of logo-name

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -45,7 +45,7 @@ export default function Greeting() {
                     download={greeting.resumeName}
                     className="download-link-button"
                   >
-                    <Button text="Download my resume" />
+                    <Button text="Resume" />
                   </a>
                 )}
               </div>
